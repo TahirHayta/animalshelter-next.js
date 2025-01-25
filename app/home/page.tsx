9ea52e4b4fc9cbb8e29ac1ce1a0c719152a54e6a
tahir <thayta22@ku.edu.tr>
2025-01-25 21:04:43 +0300

    feat: exported and added figma components, just to learn how.

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,11 +1,22 @@
 import MyAppBar from '@/components/MyAppBar'
 import { Box } from '@mui/material'
 import React from 'react'
+import figmaButton from '@/fromFigma/Button.svg'
+import Image from 'next/image'
+import CardsSection from '@/fromFigma/CardsSection.svg'
 
 const Home = () => {
   return (
-  <Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh'}}>
-    <h1 style={{ fontSize: '5rem' }}>Home</h1>
+  <Box sx={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', height: '100vh'}}>
+    <Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+      <h1 style={{ fontSize: '5rem' }}>Home</h1>
+      <div>
+        <Image src={figmaButton} alt="figmaButton" />
+      </div>
+    </Box>
+    <div>
+      <Image src={CardsSection} alt="CardsSection" />
+    </div> 
   </Box>
   )
 }
